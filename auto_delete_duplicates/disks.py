e52efe30519d1608b3bc66545def006cf6ecc51c
miska214 <167099026+miska214@users.noreply.github.com>
2026-05-25 19:59:58 +0700

    Add files via upload
    авто поиск дубликатов и их удаление

---
+++ auto_delete_duplicates/disks.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+
+def get_system_disks():
+    black_list = {
+        "Windows",
+        "Program Files",
+        "Program Files (x86)",
+        "$Recycle.Bin",
+        "AppData",
+        "System Volume Information",
+        "ProgramData",
+        "$WinREAgent",
+        "Boot",
+        "EFI",
+        "node_modules",
+        ".git",
+        ".venv",
+        "venv",
+        "Config.Msi",
+        "Recovery",
+        "MSOCache",
+    }
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    found_disks = []
+
+    for letter in letters:
+        pc_disk = Path(f"{letter}:/")
+        if pc_disk.exists():
+            found_disks.append(pc_disk)
+
+    return found_disks, black_list
